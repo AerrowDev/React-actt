f260b6dac3092ff64e5f8e78b22052ca28d1b039
AerrowDev <sgaeprellegera@gmail.com>
2023-01-29 00:05:26 +0800

    first commit

---
+++ test1/src/App.js
@@ -0,0 +1,21 @@
+import Appbar from './Appbar';
+import './App.css';
+import Demo from './Demo';
+import SignUp from './SignUp';
+
+
+
+
+
+function App() {
+  return (
+    <div className="App">
+  <Appbar/>
+  <Demo/>
+  
+    <SignUp/>
+    </div>
+  );
+}
+
+export default App;
